ffd93e202e8eadeb12ac7866090fb726ec8c896f
VAMSHI VADE. <vamshiv@STRING.METAVERSE>
2025-04-25 10:53:32 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+const PORT = 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hey this is my API running 🥳");
+});
+
+app.get("/about", (req, res) => {
+  res.send("This is my about route..... ");
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on port ${PORT}`);
+});
